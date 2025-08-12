264bba73e6a34391276fcd6f048573a57b9a3eca
vidhigoyal-startappss <vidhi.goyal@startappss.com>
2025-08-12 12:42:05 +0530

    added the tasks of week 2 with updated css

--- Day6/script.js
+++ Day6/script.js
@@ -1,43 +1,36 @@
-let display = document.getElementById("display");
+function dis(val) {
+			document.getElementById("result").value += val
+		}
 
-function appendNumber(num) {
-  if (display.innerText === "0" || display.innerText === "Error") {
-    display.innerText = num;
-  } else {
-    display.innerText += num;
-  }
-}
+		function myFunction(event) {
+			if (event.key == '0' || event.key == '1'
+				|| event.key == '2' || event.key == '3'
+				|| event.key == '4' || event.key == '5'
+				|| event.key == '6' || event.key == '7'
+				|| event.key == '8' || event.key == '9'
+				|| event.key == '+' || event.key == '-'
+				|| event.key == '*' || event.key == '/')
+				document.getElementById("result").value += event.key;
+		}
 
-function appendOperator(op) {
-  const lastChar = display.innerText.slice(-1);
-  if ("+-*/".includes(lastChar)) {
-    display.innerText = display.innerText.slice(0, -1) + op;
-  } else {
-    display.innerText += op;
-  }
-}
+		let cal = document.getElementById("calcu");
+		cal.onkeyup = function (event) {
+			if (event.keyCode === 13) {
+				console.log("Enter");
+				let x = document.getElementById("result").value
+				console.log(x);
+				solve();
+			}
+		}
 
-function clearDisplay() {
-  display.innerText = "0";
-}
+		// Function that evaluates the digit and return result
+		function solve() {
+			let x = document.getElementById("result").value
+			let y = math.evaluate(x)
+			document.getElementById("result").value = y
+		}
 
-function backspace() {
-  if (display.innerText.length > 1) {
-    display.innerText = display.innerText.slice(0, -1);
-  } else {
-    display.innerText = "0";
-  }
-}
-
-function calculateResult() {
-  try {
-    const result = eval(display.innerText);
-    if (result === Infinity || isNaN(result)) {
-      display.innerText = "Error";
-    } else {
-      display.innerText = result;
-    }
-  } catch (e) {
-    display.innerText = "Error";
-  }
-}
+		// Function that clear the display
+		function clr() {
+			document.getElementById("result").value = ""
+		}
